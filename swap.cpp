6ad33a9b227fe0d57ae034a9341ca933b5725194
cute-walk <43268894+cute-walk@users.noreply.github.com>
2020-05-12 18:22:17 +0800

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void swap(int *x, int *y);
+int main()
+{
+        int x, y;
+        std::cout << "请输入两个不同的值：";
+        std::cin >> x >> y;
+        swap (&x, &y);
+        std::cout << "调换后输出：" << x << ' ' << y << std::endl;
+
+}
+void swap(int *x, int *y)
+{
+        int temp;
+        temp = *x;
+        *x = *y;
+        *y = temp;
+}
