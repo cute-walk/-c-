0acb4cb192e9b5cde9337478301d3cd4fb4719b0
cute-walk <43268894+cute-walk@users.noreply.github.com>
2020-07-27 10:41:22 +0800

    Create that.cpp

---
+++ 0727_linking_and_scope_of_variables/that.cpp
@@ -0,0 +1,25 @@
+#include "header.h"
+
+unsigned short thatNum = 8;
+bool printMe = true;
+
+unsigned long returnFactorial(unsigned short num)
+{
+        unsigned long sum = 1;
+
+        for (int i=1; i<=num; i++)
+        {
+                sum = sum * i;
+        }
+
+        if (printMe)
+        {
+                return sum;
+        }
+
+        else
+        {
+                return 0;
+        }
+
+}
